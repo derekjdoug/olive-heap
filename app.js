bcc5111850ea2520b03988742cbd817d135eabc4
Derek Douglas <derek.j.douglas13@gmail.com>
2022-01-29 16:38:44 -0600

    added javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let name = prompt('What is your name?');
+let time = prompt('What time is it? Hint: Military Time');
+let message;
+
+if(time <=1100){
+    message = 'Good Morning!';
+} else if (time <= 1800){
+    message - 'Good Afternoon!';
+} else if (time <=2400){
+    message = 'Good Evening!';
+} else if (time >2400){
+    message = 'Time does not exist!';
+}
+
+document.write('Hello ' + name + '! ' + message);
