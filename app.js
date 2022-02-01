5e0d4431c2784f88be53108fe995295138ad2ac1
Derek Douglas <derek.j.douglas13@gmail.com>
2022-01-31 23:49:24 -0600

    updates to JS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let time = prompt('What time is it? Hint: Military Time');
 let message;
 
 function greeting(){
-if(time <=1100){
+  if(time <=1100){
     message = 'Good Morning, ';
 } else if (time <= 1800){
     message = 'Good Afternoon, ';
@@ -20,3 +20,16 @@ if(time <=1100){
     message = 'Time does not exist,';
 } return message;
 }
+
+/*
+let disposition = prompt('Do you like olives?');
+
+function olives(){
+   if(disposition.toLowerCase = 'yes'){
+       reply = 'Great!';
+   else if(disposition.toLowerCase = 'no'){
+        reply = 'I have more to learn!';
+   }
+
+}
+*/
